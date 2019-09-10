fb578ecdcfffea1acd10366eb7e47843c35c3d28
charleyarby <charleyarby@gmail.com>
2019-09-09 19:57:01 -0700

    finished something

--- src/dancer.js
+++ src/dancer.js
@@ -1,32 +1,58 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
-
-  var dancer = {};
-
+  // console.log('dancer',makeDancer.prototype);
+  //var dancer = {};
+  debugger
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  // var boundNode = function() {
+  //   this.$node = $('<span class="dancer"></span>');
+  // }.bind(makeDancer);
+  // boundNode();
+  this.$node = $('<span class="dancer"></span>');
 
-  dancer.step = function() {
+
+  this.step = function() {
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
+    var boundNode = function() {
+      this.step;
+      console.log('inside bound', this)
+    }.bind(makeDancer);
 
-  dancer.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
+    // console.log(this)
+    setTimeout(boundNode(), timeBetweenSteps);
+    //this.step.bind(makeDancer);
   };
+  this.step();
+
+  // dancer.setPosition = function(top, left) {
+  //   // Use css top and left properties to position our <span> tag
+  //   // where it belongs on the page. See http://api.jquery.com/css/
+  //   //
+  //   var styleSettings = {
+  //     top: top,
+  //     left: left
+  //   };
+  //   dancer.$node.css(styleSettings);
+  // };
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
+  //var boundSetPosition = makeDancer.prototype.setPosition.bind(makeDancer)
+  //boundSetPosition(top, left);
 
-  return dancer;
+  this.setPosition(top,left)
+  //return dancer;
+};
+
+makeDancer.prototype.setPosition = function(top, left) {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  // console.log('css', this)
+  this.$node.css(styleSettings);
 };
